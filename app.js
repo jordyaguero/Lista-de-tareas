f3bcb3e902c41c3731be620e8a15ddeab226b968
jordyaguero <jordytiradotorres@hotmail.com>
2018-01-22 18:50:24 -0500

    initial project addes tasks

---
+++ app.js
@@ -0,0 +1,28 @@
+let app = new Vue({
+    el: '#app',
+    data: {
+        title: 'Lista de tareas',
+        tareas: [{
+                name: 'Aprender node',
+                checked: false
+            },
+            {
+                name: 'Aprender css avanzando',
+                checked: true
+            }
+        ],
+        newTarea: ''
+    },
+    methods: {
+        add() {
+            let tarea = this.newTarea.trim();
+
+            this.tareas.push({
+                name: tarea,
+                checked: false
+            });
+            this.newTarea = '';
+        },
+        eliminar()
+    }
+});
