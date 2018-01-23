39b26196a3cd6b20d425c81cf5aa7123bfeb61a3
jordyaguero <jordytiradotorres@hotmail.com>
2018-01-23 18:24:34 -0500

    cambiando la aplicacion a componentes

--- app.js
+++ app.js
@@ -1,17 +1,53 @@
-let app = new Vue({
-    el: '#app',
-    data: {
-        title: 'Lista de tareas',
-        tareas: [{
-                name: 'Aprender node',
-                checked: false
-            },
-            {
-                name: 'Aprender css avanzando',
-                checked: true
-            }
-        ],
-        newTarea: ''
+let data = {
+    title: 'Lista de tareas',
+    tareas: [{
+            name: 'Aprender node',
+            checked: false
+        },
+        {
+            name: 'Aprender css avanzando',
+            checked: true
+        }
+    ],
+    newTarea: ''
+};
+
+// declarando componentes
+let ItemsComponent = Vue.extend({
+    data() {
+        return data;
+    },
+    template: `<ul class="list-group">
+                <li class="list-group-item" v-for="(tarea, index) in tareas" :class="{'removed': tarea.checked}">
+                    <div class="row">
+                        <div class="col-sm-10">
+                            <input type="checkbox" v-model="tarea.checked"> {{tarea.name}}
+                        </div>
+
+                        <span class="input-group-btn  col-sm-2  text-right">
+                            <button class="btn  btn-danger" type="button" @click="eliminar(index)">Delete</button>
+                        </span>
+                    </div>
+                </li>
+            </ul>
+        `,
+    methods: {
+        eliminar(tarea) {
+            this.tareas.splice(tarea, 1);
+        }
+    }
+});
+
+let ChangeTitleComponent = Vue.extend({
+    data() {
+        return data;
+    },
+    template: '<input type="text" class="form-control" v-model="title">'
+});
+
+let AddItemsComponent = Vue.extend({
+    data() {
+        return data;
     },
     methods: {
         add() {
@@ -22,9 +58,24 @@ let app = new Vue({
                 checked: false
             });
             this.newTarea = '';
-        },
-        eliminar(tarea) {
-            this.tareas.splice(tarea, 1);
         }
-    }
+    },
+    template: `
+            <div class="input-group">
+                <input class="form-control" type="text" placeholder="Add List" v-model="newTarea" @keyup.enter="add()">
+                <div class="input-group-append">
+                    <button class="btn  btn-primary  btn-md" type="button" @click="add()">Add</button>
+                </div>
+            </div>
+        `
+});
+
+
+Vue.component('items-component', ItemsComponent);
+Vue.component('change-title-component', ChangeTitleComponent);
+Vue.component('add-items-component', AddItemsComponent);
+
+let app = new Vue({
+    el: '#app',
+    data: data,
 });
